4f7267d92e612f55dd1b9cda9c95914d5766ec1b
jjung <jjung@waikato.ac.nz>
2019-02-27 00:27:13 +1300

    Add MethodArgumentNotValidException Exception Handler

--- src/main/java/com/github/devcat24/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/devcat24/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.github.devcat24.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -46,6 +47,11 @@ public class GlobalExceptionHandler {
         return mav;
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidExceptionHandler(Exception ex,  WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
