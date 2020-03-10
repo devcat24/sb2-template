fa3575a0a4986ef04c68258738e41ffb9d98d449
jjung <jjung@waikato.ac.nz>
2020-03-10 16:03:06 +1300

    Fix ImageResizerTest targetPath issue

--- src/test/java/com/github/devcat24/util/image/ImageResizerTest.java
+++ src/test/java/com/github/devcat24/util/image/ImageResizerTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(/*webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT*/)
 public class ImageResizerTest {
@@ -16,11 +18,17 @@ public class ImageResizerTest {
     public void testImageResizer() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("./static/images/spring_logo.jpg").getFile());
+
         String srcFilePath = file.getAbsolutePath();
-        String targetFilePath = "/home/tomcat/Downloads/spring_logo_300.jpg";
+        //String targetFilePath = "/home/tomcat/Downloads/spring_logo_300.jpg";
+        String targetFilePath = "target/spring_logo_300.jpg";
 
         ImageResizer resizer = new ImageResizer();
         resizer.resize(srcFilePath, targetFilePath, 300);
+
+        double originalFileSize = file.length()/1024;
+        double compressedFileSize = (new File(targetFilePath)).length()/1024;
+        assertTrue("Compressed image (" +compressedFileSize+ ") is smaller than original (" + originalFileSize + "kb)", originalFileSize > compressedFileSize);
 
 
     }
